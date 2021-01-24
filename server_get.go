adca97da2f75364400b36bd62b78b1a2bea9f77e
okuda <aru@okudanoMacBook-Pro.local>
2021-01-24 18:18:04 +0900

    echo API_SERVER

---
+++ server_get.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/user", show)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func show(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, u)
+}
